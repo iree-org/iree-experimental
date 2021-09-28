61b578cd45c6355ce7d2c3d5ff4a5afeba33a78d
Rob Suderman <rob.suderman@gmail.com>
2021-09-28 14:29:31 -0700

    [tfhub] Added bird classifier example
    Bird classifier is quantized so this is handy.

---
+++ tflitehub/bird_classifier_test.py
@@ -0,0 +1,21 @@
+
+import absl.testing
+import numpy
+import test_util
+
+model_path = "https://tfhub.dev/google/lite-model/aiy/vision/classifier/birds_V1/3?lite-format=tflite"
+
+class PoseTest(test_util.TFLiteModelTest):
+  def __init__(self, *args, **kwargs):
+    super(PoseTest, self).__init__(model_path, *args, **kwargs)
+
+  def compare_results(self, iree_results, tflite_results, details):
+    super(PoseTest, self).compare_results(iree_results, tflite_results, details)
+    self.assertTrue(numpy.isclose(iree_results[0], tflite_results[0], atol=1e-3).all())
+
+  def test_compile_tflite(self):
+    self.compile_and_execute()
+
+if __name__ == '__main__':
+  absl.testing.absltest.main()
+
