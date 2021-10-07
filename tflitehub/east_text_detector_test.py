62b03d609a1ae2a6ce94cce4228ebd9aeefb0eb2
Rob Suderman <rob.suderman@gmail.com>
2021-10-06 17:30:36 -0700

    Added numerical checks to lightning, east, and person detect
    Person detect is now passing.

--- tflitehub/east_text_detector_test.py
+++ tflitehub/east_text_detector_test.py
@@ -2,6 +2,7 @@
 # XFAIL: *
 
 import absl.testing
+import numpy
 import test_util
 
 model_path = "https://tfhub.dev/sayakpaul/lite-model/east-text-detector/dr/1?lite-format=tflite"
@@ -12,6 +13,8 @@ class EastTextDetectorTest(test_util.TFLiteModelTest):
 
   def compare_results(self, iree_results, tflite_results, details):
     super(EastTextDetectorTest, self).compare_results(iree_results, tflite_results, details)
+    self.assertTrue(numpy.isclose(iree_results[0], tflite_results[0], atol=1e-3).all())
+    self.assertTrue(numpy.isclose(iree_results[1], tflite_results[1], atol=1e-3).all())
 
   def test_compile_tflite(self):
     self.compile_and_execute()
