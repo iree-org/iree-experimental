4f24d97cfd56d30248e1659644644f2ac6bfa0e6
Julian Walker <julianreidwalker@gmail.com>
2021-10-07 23:16:14 +0000

    Fix setup_env.sh to use user-specificed Python path

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,27 +3,27 @@
 # Recommend getting default 'python' to be python 3. For example on Debian:
 #   sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 # Or launch with python=/some/path
-td="$(cd $(dirname $0) && pwd)"
-venv_dir="$td/iree-samples.venv"
-if [ -z "$python" ]; then
-  python="$(which python)"
+TD="$(cd $(dirname $0) && pwd)"
+VENV_DIR="$TD/iree-samples.venv"
+if [ -z "$PYTHON" ]; then
+  PYTHON="$(which python)"
 fi
 
-echo "Setting up venv dir: $venv_dir"
-echo "Python: $python"
-echo "Python version: $("$python" --version)"
+echo "Setting up venv dir: $VENV_DIR"
+echo "Python: $PYTHON"
+echo "Python version: $("$PYTHON" --version)"
 
 function die() {
   echo "Error executing command: $*"
   exit 1
 }
 
-python -m venv "$venv_dir" || die "Could not create venv."
-source "$venv_dir/bin/activate" || die "Could not activate venv"
+$PYTHON -m venv "$VENV_DIR" || die "Could not create venv."
+source "$VENV_DIR/bin/activate" || die "Could not activate venv"
 
 # Upgrade pip.
-python -m pip install --upgrade pip || die "Could not upgrade pip"
-python -m pip install --upgrade -r "$td/requirements.txt"
+$PYTHON -m pip install --upgrade pip || die "Could not upgrade pip"
+$PYTHON -m pip install --upgrade -r "$TD/requirements.txt"
 
 echo "Activate venv with:"
-echo "  source $venv_dir/bin/activate"
+echo "  source $VENV_DIR/bin/activate"
