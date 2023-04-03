1feaef4e29ec22daae7a9c49e7f279aacd3546c4
mariecwhite <mariewhite@google.com>
2023-04-02 22:42:09 -0700

    Add TF Bert-Large models

--- iree-tf/library/generate_saved_models.py
+++ iree-tf/library/generate_saved_models.py
@@ -3,11 +3,14 @@ import numpy as np
 import os
 import tensorflow as tf
 
-from models import resnet50
+from models import resnet50, bert_large
 
 _MODEL_NAME_TO_MODEL_CONFIG = {
     # Batch sizes taken from MLPerf A100 Configs: https://github.com/mlcommons/inference_results_v2.1/tree/master/closed/NVIDIA/configs/resnet50
     "RESNET50": (resnet50.ResNet50, [1, 8, 64, 128, 256, 2048]),
+    # Batch sizes based on MLPerf config: https://github.com/mlcommons/inference_results_v2.1/tree/master/closed/NVIDIA/configs/bert
+    "BERT_LARGE":
+    (bert_large.BertLarge, [1, 16, 24, 32, 48, 64, 512, 1024, 1280]),
 }
 
 if __name__ == "__main__":
@@ -37,13 +40,13 @@ if __name__ == "__main__":
                 # Save inputs.
                 for idx, input in enumerate(inputs):
                     input_path = os.path.join(save_dir, f"input_{idx}.npy")
-                    print(f"Saving input {idx} to {input_path}")
+                    print(f"Saving input {input.get_shape()} to {input_path}")
                     np.save(input_path, input)
 
                 # Save output.
                 outputs = model.forward(*inputs)
                 output_path = os.path.join(save_dir, f"output_0.npy")
-                print(f"Saving output 0 to {output_path}")
+                print(f"Saving output {outputs.get_shape()} to {output_path}")
                 np.save(output_path, outputs)
 
                 # Save saved model.
@@ -54,7 +57,7 @@ if __name__ == "__main__":
 
                 saved_model_path = os.path.join(save_dir, "saved_model")
                 os.makedirs(saved_model_path, exist_ok=True)
-                print(f"Saving {saved_model_path}")
+                print(f"Saving {saved_model_path} with call signature: {call_signature}")
                 tf.saved_model.save(model, saved_model_path,
                                     signatures={"serving_default": call_signature})
             except Exception as e:
