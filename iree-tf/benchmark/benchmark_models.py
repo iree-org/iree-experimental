06bd5ad0139ebe5298836b34e93e460bd71243c0
mariecwhite <mariewhite@google.com>
2023-04-11 15:55:12 -0700

    Log peak memory usage for TF

--- iree-tf/benchmark/benchmark_models.py
+++ iree-tf/benchmark/benchmark_models.py
@@ -2,9 +2,11 @@ import argparse
 import pathlib
 import statistics
 import sys
+import tensorflow as tf
 import time
 
 from multiprocessing import Process
+from typing import Optional
 
 # Add library dir to the search path.
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent / "library"))
@@ -20,38 +22,56 @@ _MODEL_NAME_TO_MODEL_CONFIG = {
     "T5_LARGE": (t5_large.T5Large, [1, 16, 24, 32, 48, 64, 512]),
 }
 
+_TF_CPU_DEVICE = "/CPU:0"
+_TF_GPU_DEVICE = "/GPU:0"
 
-def write_line(file_path, text, append=True):
+def write_line(file_path: str, text: str, append: bool = True) -> None:
     with open(file_path, "a" if append else "w") as f:
         print(f"Writing {text}")
         f.write(text + "\n")
 
+def bytes_to_mb_str(bytes: Optional[int]) -> str:
+    return "n/a" if bytes is None else f"{bytes / 1e6:.6f}"
 
-def run_benchmark(model_name, model_class, batch_size, warmup_iterations,
-                  benchmark_iterations, csv_path):
+def run_benchmark(model_name: str, model_class: type[tf.Module], batch_size: int,
+                  warmup_iterations: int, benchmark_iterations: int,
+                  tf_device: str, csv_path: str) -> None:
     try:
-        model = model_class()
-        inputs = model.generate_inputs(batch_size)
+        with tf.device(tf_device):
+            if tf_device == _TF_GPU_DEVICE:
+                tf.config.experimental.reset_memory_stats(tf_device)
+            
+            model = model_class()
+            inputs = model.generate_inputs(batch_size)
 
-        # Run warmup.
-        warmup_latencies = []
-        for i in range(warmup_iterations):
-            start = time.perf_counter()
-            model.forward(*inputs)
-            latency = 1000 * (time.perf_counter() - start)
-            warmup_latencies.append(latency)
+            # Run warmup.
+            warmup_latencies = []
+            for i in range(warmup_iterations):
+                start = time.perf_counter()
+                model.forward(*inputs)
+                latency = 1000 * (time.perf_counter() - start)
+                warmup_latencies.append(latency)
 
-        # Run benchmark.
-        latencies = []
-        for i in range(benchmark_iterations):
-            start = time.perf_counter()
-            model.forward(*inputs)
-            latency = 1000 * (time.perf_counter() - start)
-            latencies.append(latency)
+            # Run benchmark.
+            latencies = []
+            for i in range(benchmark_iterations):
+                start = time.perf_counter()
+                model.forward(*inputs)
+                latency = 1000 * (time.perf_counter() - start)
+                latencies.append(latency)
 
-        # Save results.
-        result = f"{model_name},{batch_size},{min(warmup_latencies)},{max(warmup_latencies)},{statistics.mean(warmup_latencies)},{statistics.median(warmup_latencies)},{statistics.stdev(warmup_latencies)},{min(latencies)},{max(latencies)},{statistics.mean(latencies)},{statistics.median(latencies)},{statistics.stdev(latencies)}"
-        write_line(csv_path, result)
+            # Retrieve memory stats.
+            if tf_device == _TF_GPU_DEVICE:
+                memory_info = tf.config.experimental.get_memory_info(tf_device)
+                device_peak_b = memory_info["peak"]
+            else:
+                 # tf.config.experimental does not currently support measuring CPU memory usage.
+                device_peak_b = None
+            device_peak_mb = bytes_to_mb_str(device_peak_b)
+
+            # Save results.
+            result = f"{model_name},{batch_size},{min(warmup_latencies)},{max(warmup_latencies)},{statistics.mean(warmup_latencies)},{statistics.median(warmup_latencies)},{statistics.stdev(warmup_latencies)},{min(latencies)},{max(latencies)},{statistics.mean(latencies)},{statistics.median(latencies)},{statistics.stdev(latencies)},{device_peak_mb}"
+            write_line(csv_path, result)
 
     except Exception as e:
         print(f"Failed to benchmark model {model_name}. Exception: {e}")
@@ -71,9 +91,14 @@ if __name__ == "__main__":
                            "--iterations",
                            default=100,
                            help="The number of iterations to benchmark.")
+    argParser.add_argument("-d",
+                           "--device",
+                           default="gpu",
+                           help="The device to run on. Currently `cpu` and `gpu` are supported.")
     args = argParser.parse_args()
+    tf_device = _TF_GPU_DEVICE if args.device == "gpu" else _TF_CPU_DEVICE
 
-    csv_header = "model,batch_size,warmup_min_ms,warmup_max_ms,warmup_mean_ms,warmup_median_ms,warmup_stddev_ms,latency_min_ms,latency_max_ms,latency_mean_ms,latency_median_ms,latency_stddev_ms"
+    csv_header = "model,batch_size,warmup_min_ms,warmup_max_ms,warmup_mean_ms,warmup_median_ms,warmup_stddev_ms,latency_min_ms,latency_max_ms,latency_mean_ms,latency_median_ms,latency_stddev_ms,device_peak_mb"
     write_line(args.output_path, csv_header, append=False)
 
     for model_name, model_config in _MODEL_NAME_TO_MODEL_CONFIG.items():
@@ -84,6 +109,6 @@ if __name__ == "__main__":
             p = Process(target=run_benchmark,
                         args=(model_name, model_class, batch_size,
                               args.warmup_iterations, args.iterations,
-                              args.output_path))
+                              tf_device, args.output_path))
             p.start()
             p.join()
