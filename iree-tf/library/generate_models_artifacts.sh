dee3fca0a69b5dcd65c8ee4ee1782c8fb97f2bc9
mariecwhite <mariewhite@google.com>
2023-04-17 00:21:19 -0700

    Run iree-import-tf and save the mlir

--- iree-tf/library/generate_models_artifacts.sh
+++ iree-tf/library/generate_models_artifacts.sh
@@ -17,22 +17,43 @@ DIR_NAME="tf_models_${TF_VERSION}_$(date +'%s')"
 OUTPUT_DIR="/tmp/${DIR_NAME}"
 mkdir ${OUTPUT_DIR}
 
-pip list > ${OUTPUT_DIR}/version_info.txt
+pip list > ${OUTPUT_DIR}/models_version_info.txt
 
-python generate_saved_models.py -o ${OUTPUT_DIR}
+python generate_models_artifacts.py -o ${OUTPUT_DIR}
 
-# Zip the saved models.
+# Generate hlo.
+# We use a different python environment because we require the latest version of Tensorflow.
+rm -rf tf-importer.venv
+bash setup_tf_importer.sh
+source tf-importer.venv/bin/activate
+
+pip list > ${OUTPUT_DIR}/importer_version_info.txt
+
 MODEL_DIRS=$(find "${OUTPUT_DIR}/" -maxdepth 1 -mindepth 1 -type d)
-
 for model_dir in ${MODEL_DIRS}; do
     BATCH_DIRS=$(find "${model_dir}/" -maxdepth 1 -mindepth 1 -type d)
     for batch_dir in ${BATCH_DIRS}; do
         SM_DIR="${batch_dir}/saved_model"
+        iree-import-tf --output-format=mlir-bytecode --tf-import-type=savedmodel_v2 --tf-savedmodel-exported-names=forward ${SM_DIR} -o "${batch_dir}/hlo.mlirbc"
+    done
+done
+
+# Zip the saved models and hlo.
+for model_dir in ${MODEL_DIRS}; do
+    BATCH_DIRS=$(find "${model_dir}/" -maxdepth 1 -mindepth 1 -type d)
+    for batch_dir in ${BATCH_DIRS}; do
+        # Zip saved models.
+        SM_DIR="${batch_dir}/saved_model"
         pushd "${SM_DIR}"
         tar -czvf ../tf-model.tar.gz .
         popd
-        # Delete the unzipped directory.
         rm -rf "${SM_DIR}"
+        # Zip hlo dump.
+        HLO_DIR="${batch_dir}/hlo"
+        pushd "${HLO_DIR}"
+        tar -czvf ../hlo.tar.gz .
+        popd
+        rm -rf "${HLO_DIR}"
     done
 done
 
