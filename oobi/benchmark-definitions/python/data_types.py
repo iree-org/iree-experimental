1c2bfe6361ee58e1f5428e918e4941f260b9e81d
mariecwhite <mariewhite@google.com>
2023-05-03 09:24:11 +1000

    Revert "Add definitions for model artifacts"
    This reverts commit ab36ced919d9313ebdf9b1c3b31653e2ef4323c0.

--- oobi/benchmark-definitions/python/data_types.py
+++ oobi/benchmark-definitions/python/data_types.py
@@ -2,20 +2,11 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import List
 
-class ModelFrameworkType(Enum):
-  """Type of framework a model is implemented in."""
+class FrameworkType(Enum):
+  """Type of model source."""
   TENSORFLOW_V1 = "tensorflow_v1"
   TENSORFLOW_V2 = "tensorflow_v2"
   PYTORCH = "framework_pt"
-
-class ModelArtifactType(Enum):
-  """Type of model artifact."""
-  TF_SAVEDMODEL_V1 = "tf_savedmodel_v1"
-  TF_SAVEDMODEL_V2 = "tf_savedmodel_v2"
-  MLIR_STABLEHLO = "mlir_stablehlo"
-  MLIR_MHLO = "mlir_mhlo"
-  MLIR_LINALG = "mlir_linalg"
-  MLIR_TOSA = "mlir_tosa"
 
 class DataFormat(Enum):
   """Model input data format."""
@@ -53,7 +44,7 @@ class Model(object):
   name: str
   # Tags that describe the model characteristics.
   tags: List[str]
-  framework_type: ModelFrameworkType
+  framework_type: FrameworkType
   # Source of the model implementation.
   source_info: str
   input_batch_size: int
@@ -62,17 +53,3 @@ class Model(object):
 
   def __str__(self):
     return self.name
-  
-@dataclass(frozen=True)
-class ModelArtifact(object):
-  """An artifact derived from a model"""
-  id: str
-  # The id of the model implementation the artifact is derived from.
-  parent_model_id: str
-  # Friendly unique name.
-  name: str
-  # Tags that describe the artifact characteristics.
-  tags: List[str]
-  artifact_type: ModelArtifactType
-  # Where to download the model artifact.
-  source_url: str
