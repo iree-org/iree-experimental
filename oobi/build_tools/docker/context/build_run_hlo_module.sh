0712dea44f19e7db3b3265f173c30f86683d68b6
mariecwhite <mariewhite@google.com>
2023-05-11 19:03:30 -0700

    Enable XLA:CPU benchmarks

--- oobi/build_tools/docker/context/build_run_hlo_module.sh
+++ oobi/build_tools/docker/context/build_run_hlo_module.sh
@@ -8,12 +8,19 @@ echo "LD_LIBRARY_PATH: $(echo ${LD_LIBRARY_PATH})"
 git clone https://github.com/tensorflow/tensorflow.git
 pushd tensorflow
 git checkout "${COMMIT_SHA}"
-bazel build -c opt --config=cuda \
+
+if [[ -n "${CUDA_VERSION}" ]]; then
+  bazel build -c opt --config=cuda \
     --action_env TF_CUDA_COMPUTE_CAPABILITIES="8.0" \
     --action_env GCC_HOST_COMPILER_PATH="/usr/bin/x86_64-linux-gnu-gcc-11" \
     --action_env LD_LIBRARY_PATH="/usr/local/cuda-${CUDA_VERSION}/lib64:" \
     --action_env CUDA_TOOLKIT_PATH="/usr/local/cuda-${CUDA_VERSION}" \
     --copt=-Wno-switch \
     tensorflow/compiler/xla/tools/run_hlo_module
+else
+  bazel build -c opt --copt=-Wno-switch \
+    --action_env GCC_HOST_COMPILER_PATH="/usr/bin/x86_64-linux-gnu-gcc-11" \
+    tensorflow/compiler/xla/tools/run_hlo_module
+fi
 
 popd
